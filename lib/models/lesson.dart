1476c4cab122815cb14a50636000c1e4bc99b45a
Brandon <familyrbbwand77@gmail.com>
2026-09-18 22:34:20 +0700

    Menambahkan class lesson  beserta aturannya

---
+++ lib/models/lesson.dart
@@ -0,0 +1,11 @@
+class Lesson {
+  final String question;
+  final List<String> options;
+  final String correctAnswer;
+
+  const Lesson({
+    required this.question,
+    required this.options,
+    required this.correctAnswer,
+  });
+}
